9ebe53f24108fd5201076727f783ca0e7161f033
zion <avakianssion@gmail.com>
2025-08-16 21:06:24 -0400

    a bunch of changes to split functions, refresh system in main, and other shit i don't remember

--- src/collector.rs
+++ src/collector.rs
@@ -2,10 +2,17 @@
 use serde_json::{Value, json};
 use sysinfo::{Disks, Networks, System};
 
+/// Function to get raw uptime.
+fn uptime_raw(sys: &System) -> String {
+    System::uptime().to_string()
+}
+/// Function to get raw cpufreq.
+fn cpu_freq_raw(sys: &System) -> String {
+    let cpu_freq = sys.cpus().first().map(|cpu| cpu.frequency()).unwrap_or(0);
+    cpu_freq.to_string()
+}
 /// Function to collect system metrics as single json object.
-pub fn get_sysinfo(mut sys: System) -> Value {
-    sys.refresh_all();
-
+pub fn get_sysinfo(sys: &System) -> Value {
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
@@ -18,21 +25,19 @@ pub fn get_sysinfo(mut sys: System) -> Value {
     json!({
         "timestamp": timestamp,
         "hostname": hostname,
-        "uptime": get_uptime(),
-        "cpu_freq_mhz": get_cpu_freq(System::new_all()),
+        "uptime": uptime_raw(sys),
+        "cpu_freq_mhz": cpu_freq_raw(sys),
         "disk_usage": get_disk_usage(),
         "network": get_if_data()
     })
 }
-/// Function to get uptime.
-pub fn get_uptime() -> String {
-    let uptime = System::uptime().to_string();
-    uptime
+/// Function to get JSON formatted uptime.
+pub fn uptime_json(sys: &System) -> Value {
+    json!({"uptime": uptime_raw(sys)})
 }
-/// Function to get cpufreq.
-pub fn get_cpu_freq(sys: System) -> String {
-    let cpu_freq = sys.cpus().first().map(|cpu| cpu.frequency()).unwrap_or(0);
-    cpu_freq.to_string()
+/// Function to get JSON formatted cpufreq.
+pub fn cpu_freq_json(sys: &System) -> Value {
+    json!({"cpu_freq_mhz": cpu_freq_raw(sys)})
 }
 /// Function to get metrics from interfaces.
 pub fn get_if_data() -> Vec<Value> {
@@ -49,7 +54,6 @@ pub fn get_if_data() -> Vec<Value> {
         })
         .collect()
 }
-
 /// Function to get disk usage information.
 pub fn get_disk_usage() -> Vec<Value> {
     let disks = Disks::new_with_refreshed_list();
