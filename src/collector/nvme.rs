f5053df12ee0ff7ee57783b9248cbeae541bf89a
zion <avakianssion@gmail.com>
2025-12-10 05:07:06 -0500

    tiny comment

--- src/collector/nvme.rs
+++ src/collector/nvme.rs
@@ -13,7 +13,7 @@ pub struct NvmesSmartLog {
     /// NVMe device name (e.g., "nvme0")
     /// Potential issue - we use u64 for all values in the struct.
     /// If a drive runs long enough or has crazy write workload, the 128-bit SMART counters might
-    /// exceed 2^64-1 so we would likely end up truncating data. 
+    /// exceed 2^64-1 so we would likely end up truncating data.
     /// TODO - consider changing u64 to u128.
     pub nvme_name: String,
 
